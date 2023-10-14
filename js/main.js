a1c605043f4e6e9cd4e10d4827d91be644389d00
Derin DeNio <derindenio@gmail.com>
2023-10-14 17:05:41 -0600

    Created our generic animal class, created Dog specialzed class, created pig specialzed class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 //Object Inheritance Practice
+
+//Creating a Farm
+ 
+   //Creating inital / generic animal class for all farm animals
+   //all of the farm animals will have a name and be able to speak:
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+
+  //create our first specialized animal class object that will extend the animal class BUT have a unique property [breed]
+
+class Dog extends Animal{
+    constructor(name,type, breed){
+        // 'super' will grab all of the properties from the Animal class; 'inherit' all the properties 
+        super(name,type)
+        this.breed = breed
+    }
+}
+
+//creating our first dog farm animal from our Dog class
+
+let simba = new Dog('Simba', 'dog','Sheperd')
+
+class Pig extends Animal {
+    constructor(name, type, color) {
+        super (name,type);
+        this.color = color
+    }
+}
+
+let porky = new Pig('Porky', 'pig','spotted')
