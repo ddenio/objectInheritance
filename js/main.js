1630fd98f8a76d93b43af4c7b4ccbf1f99b8e18b
Derin DeNio <derindenio@gmail.com>
2023-10-16 15:10:22 -0600

    Added 1 more poroperty to my Animal Class, added a LLama subclass

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
 //Creating a Farm
  
    //Creating inital / generic animal class for all farm animals
-   //all of the farm animals will have a name and be able to speak:
+   //all of the farm animals will have a name, type, and be able to speak:
 class Animal {
-    constructor(name, type) {
+    constructor(name, type, numOfLegs) {
         this.name = name;
         this.type = type;
+        this.numOfLegs = numOfLegs;
     }
     speak(){
         console.log(`${this.name} makes a sound`)
@@ -17,22 +18,34 @@ class Animal {
   //create our first specialized animal class object that will extend the animal class BUT have a unique property [breed]
 
 class Dog extends Animal{
-    constructor(name,type, breed){
+    constructor(name,type, numOfLegs, breed){
         // 'super' will grab all of the properties from the Animal class; 'inherit' all the properties 
-        super(name,type)
+        super(name,type, numOfLegs)
         this.breed = breed
     }
 }
 
 //creating our first dog farm animal from our Dog class
 
-let simba = new Dog('Simba', 'dog','Sheperd')
+let simba = new Dog('Simba', 'dog', 4, 'Sheperd');
 
 class Pig extends Animal {
-    constructor(name, type, color) {
-        super (name,type);
+    constructor(name, type, numOfLegs,color) {
+        super (name,type,numOfLegs);
         this.color = color
     }
 }
 
-let porky = new Pig('Porky', 'pig','spotted')
+let porky = new Pig('Porky', 'pig', 4, 'spotted');
+
+class LLama extends Animal {
+    constructor(name, type, numOfLegs) {
+        super(name,type,numOfLegs);
+        
+    }
+    spit(){
+        console.log(`${this.name}, the llama spits at owner!`)
+    }
+}
+
+let spot = new LLama('Grumpy', 'llama', 4)
